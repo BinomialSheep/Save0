c615ff9d9cc34fcf1343e61b29f7fc553be79acf
BinomialSheep <jose_jinrou@yahoo.co.jp>
2025-10-16 23:38:18 +0900

    first commit

---
+++ main copy.py
@@ -0,0 +1,198 @@
+def go_to_start():
+	while get_pos_x() > 0:
+		move(West)
+	while get_pos_y() > 0:
+		move(South)
+
+
+# todo: はみ出してワープした方が早い場合がある
+def go_to_xy(x, y):
+	while get_pos_x() > x:
+		move(West)
+	while get_pos_x() < x:
+		move(East)
+	while get_pos_y() > y:
+		move(South)
+	while get_pos_y() < y:
+		move(North)
+
+
+def harvest_glass(num=6):
+	for i in range(num):
+		for j in range(get_world_size()):
+			while not can_harvest():
+				pass
+			harvest()
+			move(North)
+	clear()
+
+
+def harvest_bush_all():
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			plant(Entities.Bush)
+			move(North)
+		move(East)
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			while not can_harvest():
+				pass
+			harvest()
+			move(North)
+		move(East)
+
+
+def harvest_tree_all():
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			if (i + j) % 2 == 0:
+				plant(Entities.Tree)
+			else:
+				plant(Entities.Bush)
+			move(North)
+		move(East)
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			while not can_harvest():
+				pass
+			harvest()
+			move(North)
+		move(East)
+
+
+def harvest_carrot_all():
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			if get_ground_type() != Grounds.Soil:
+				till()
+			plant(Entities.Carrot)
+			move(North)
+		move(East)
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			while not can_harvest():
+				pass
+			harvest()
+			move(North)
+		move(East)
+
+
+# サボテン
+def harvest_cactus_all():
+	size = get_world_size()
+	for i in range(size):
+		for j in range(size):
+			if get_ground_type() != Grounds.Soil:
+				till()
+			plant(Entities.Cactus)
+			move(North)
+		move(East)
+	# 列のソートをする
+	for i in range(size):
+		for j in range(size):
+			prev = -1
+			ok = True
+			for k in range(size):
+				cur = measure()
+				if prev > cur:
+					ok = False
+					swap(South)
+				prev = cur
+				move(North)
+			if ok:
+				break
+		move(East)
+	# 行のソートをする
+	for i in range(size):
+		for j in range(size):
+			prev = -1
+			ok = True
+			for k in range(size):
+				cur = measure()
+				if prev > cur:
+					ok = False
+					swap(West)
+				prev = cur
+				move(East)
+			if ok:
+				break
+		move(North)
+	# 収穫
+	go_to_start()
+	if can_harvest():
+		harvest()
+
+
+def watering_all():
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			use_item(Items.Water)
+			move(North)
+		move(East)
+
+
+def plant_pumpkin():
+	# 最初の埋め
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			if get_ground_type() != Grounds.Soil:
+				till()
+			plant(Entities.Pumpkin)
+			move(North)
+		move(East)
+	for i in range(get_world_size()):
+		# 2回目は適当に埋める
+		for j in range(get_world_size()):
+			if get_entity_type() == Entities.Dead_Pumpkin:
+				plant(Entities.Pumpkin)
+			move(North)
+		# 3回目以降は待つ
+		for j in range(get_world_size()):
+			while True:
+				if can_harvest():
+					break
+				elif get_entity_type() != Entities.Dead_Pumpkin:
+					# パンプキンが育っている途中なので待つ
+					do_a_flip()
+				else:
+					plant(Entities.Pumpkin)
+			move(North)
+		move(East)
+	if can_harvest():
+		harvest()
+
+
+def harvest_all():
+	go_to_start()
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			if can_harvest():
+				harvest()
+			move(North)
+		move(East)
+
+
+# go_to_start()
+# harvest_all()
+def main():
+	loop_count = 0
+	if can_harvest():
+		harvest()
+	# watering_all()
+	clear()
+	# harvest_cactus_all()
+	while True:
+		clear()
+		harvest_glass()
+		if loop_count % 10 == 0:
+			plant_pumpkin()
+		elif loop_count % 4 == 0:
+			harvest_carrot_all()
+		else:
+			harvest_tree_all()
+		loop_count += 1
+
+
+if __name__ == "__main__":
+	main()
+	
